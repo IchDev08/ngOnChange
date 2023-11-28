02d7c67b415b681b6017639dd101e7d730c440ac
ichnhanh <ichnh@nhanh.vn>
2023-11-28 10:18:01 +0700

    [ngOnChange]: - khởi tạo

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent implements OnChanges {
+
+  @Input() name: string = ''
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes['name'])
+  }
+
+}
